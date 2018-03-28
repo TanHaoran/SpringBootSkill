c60a3739df342ee6d071a6f65eff0781f54ed191
JerryTan <thrforever@126.com>
2018-03-28 23:13:41 +0800

    完成至8.2

---
+++ src/main/java/com/jerry/springbootskill/domain/SysUser.java
@@ -0,0 +1,314 @@
+package com.jerry.springbootskill.domain;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Table(name = "sys_user")
+public class SysUser {
+    @Id
+    private String id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 昵称
+     */
+    private String nickname;
+
+    private Integer age;
+
+    private Integer sex;
+
+    private Integer job;
+
+    @Column(name = "face_image")
+    private String faceImage;
+
+    private String province;
+
+    private String city;
+
+    /**
+     * 地区
+     */
+    private String district;
+
+    private String address;
+
+    @Column(name = "auth_salt")
+    private String authSalt;
+
+    @Column(name = "last_login_ip")
+    private String lastLoginIp;
+
+    @Column(name = "last_login_time")
+    private Date lastLoginTime;
+
+    @Column(name = "is_delete")
+    private Integer isDelete;
+
+    @Column(name = "register_time")
+    private Date registerTime;
+
+    /**
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取用户名
+     *
+     * @return username - 用户名
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * 设置用户名
+     *
+     * @param username 用户名
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * 获取密码
+     *
+     * @return password - 密码
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置密码
+     *
+     * @param password 密码
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获取昵称
+     *
+     * @return nickname - 昵称
+     */
+    public String getNickname() {
+        return nickname;
+    }
+
+    /**
+     * 设置昵称
+     *
+     * @param nickname 昵称
+     */
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    /**
+     * @return age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * @param age
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    /**
+     * @return sex
+     */
+    public Integer getSex() {
+        return sex;
+    }
+
+    /**
+     * @param sex
+     */
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * @return job
+     */
+    public Integer getJob() {
+        return job;
+    }
+
+    /**
+     * @param job
+     */
+    public void setJob(Integer job) {
+        this.job = job;
+    }
+
+    /**
+     * @return face_image
+     */
+    public String getFaceImage() {
+        return faceImage;
+    }
+
+    /**
+     * @param faceImage
+     */
+    public void setFaceImage(String faceImage) {
+        this.faceImage = faceImage;
+    }
+
+    /**
+     * @return province
+     */
+    public String getProvince() {
+        return province;
+    }
+
+    /**
+     * @param province
+     */
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    /**
+     * @return city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * 获取地区
+     *
+     * @return district - 地区
+     */
+    public String getDistrict() {
+        return district;
+    }
+
+    /**
+     * 设置地区
+     *
+     * @param district 地区
+     */
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    /**
+     * @return address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return auth_salt
+     */
+    public String getAuthSalt() {
+        return authSalt;
+    }
+
+    /**
+     * @param authSalt
+     */
+    public void setAuthSalt(String authSalt) {
+        this.authSalt = authSalt;
+    }
+
+    /**
+     * @return last_login_ip
+     */
+    public String getLastLoginIp() {
+        return lastLoginIp;
+    }
+
+    /**
+     * @param lastLoginIp
+     */
+    public void setLastLoginIp(String lastLoginIp) {
+        this.lastLoginIp = lastLoginIp;
+    }
+
+    /**
+     * @return last_login_time
+     */
+    public Date getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    /**
+     * @param lastLoginTime
+     */
+    public void setLastLoginTime(Date lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    /**
+     * @return is_delete
+     */
+    public Integer getIsDelete() {
+        return isDelete;
+    }
+
+    /**
+     * @param isDelete
+     */
+    public void setIsDelete(Integer isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    /**
+     * @return register_time
+     */
+    public Date getRegisterTime() {
+        return registerTime;
+    }
+
+    /**
+     * @param registerTime
+     */
+    public void setRegisterTime(Date registerTime) {
+        this.registerTime = registerTime;
+    }
+}
---
+++ src/main/java/com/jerry/springbootskill/mapper/SysUserMapper.java
@@ -0,0 +1,7 @@
+package com.jerry.springbootskill.mapper;
+
+import com.jerry.springbootskill.domain.SysUser;
+import com.jerry.springbootskill.util.MyMapper;
+
+public interface SysUserMapper extends MyMapper<SysUser> {
+}
